b15aa2767461dff140de87cb13e248aa17479170
Michael Bestas <mkbestas@lineageos.org>
2020-12-27 18:08:09 +0200

    daisy: Also set AB_OTA_UPDATER before including sakura.mk
    in order for device/xiaomi/msm8953-common/msm8953.mk checks to work
    
    Change-Id: I578c171da1a49d3556b295ea132b69b2603941aa

--- device.mk
+++ device.mk
@@ -12,6 +12,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # A/B
+AB_OTA_UPDATER := true
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
