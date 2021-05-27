484fa978420ee28f9543ead801ce06a08b83a52a
ItsVixano <vixanonew@gmail.com>
2021-05-27 16:47:19 +0200

    daisy: Nuke Touch hal
    * For some reasons, it doesn't start with enforcing
    * Also i haven't found anything useful regard it "at least on our device" ...
    
    Change-Id: I4c4bfb7f914a7808d9c3b13b1b6c2ac89fd14f95

--- device.mk
+++ device.mk
@@ -380,10 +380,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8953
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
