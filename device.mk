e67e050049126c3239b0765921f72b7726f85cf6
Giovanni Ricca <giovanniricca@proton.me>
2023-02-19 17:41:52 +0100

    daisy: Import device-specific audio configs
    Change-Id: I34c513cab34557dfe0248f0d6129b14282853a39

--- device.mk
+++ device.mk
@@ -16,6 +16,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
 # A/B
 AB_OTA_UPDATER := true
 
